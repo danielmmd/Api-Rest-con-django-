dd6f732830cf2714d0e5b96f5416ac6827a79e8a
Daniel <danielmmd24@gmail.com>
2023-06-24 22:05:00 -0500

    nada que lees eso

--- crudsimple/settings.py
+++ crudsimple/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    'django-cors-headers', 
     'Persona',
     'rest_framework',
 ]
