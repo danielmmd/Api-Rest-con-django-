eda9022bb08a147dc0b05a135c55c4b6148e0cbe
Daniel <danielmmd24@gmail.com>
2023-06-24 21:35:55 -0500

    activacion de cors

--- crudsimple/settings.py
+++ crudsimple/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'Persona',
     'rest_framework',
 ]
@@ -55,8 +56,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     
 ]
+
+CORS_ORIGIN_WHITELIST = (
+
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://localhost'
+
+)
+
 
 ROOT_URLCONF = 'crudsimple.urls'
 
